40078862b6d5382042aede6c0e92e232a102d777
Dan <danpopa1998@gmail.com>
2019-02-08 19:49:46 +0200

    play sound on player page mount

--- pages/PlayerPage.js
+++ pages/PlayerPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text } from "react-native";
 import Player from "./playerPage/Player";
+import { Player as PlayerSound } from "react-native-audio-toolkit";
 
 export default class PlayerPage extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -9,12 +10,34 @@ export default class PlayerPage extends React.Component {
     };
   };
 
+  state = {
+    isPlaying: false
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    const path = navigation.getParam("path", null);
+
+    this.sound = new PlayerSound(path, {
+      autoDestroy: true,
+      continuesToPlayInBackground: true
+    }).play(err => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({ isPlaying: true });
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    this.sound.destroy();
+  }
+
   render() {
     const { navigation } = this.props;
     const path = navigation.getParam("path", null);
     const name = navigation.getParam("name", "No Name");
-    return (
-      <Player />
-    );
+    return <Player />;
   }
 }
